bc7b9c1152afe39d8254d0ab19a2eaf7a9fbfe36
Tejas Pawar <tejaspawar172000@gmail.com>
2025-11-01 09:42:00 +0530

    Test changes

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -4,26 +4,26 @@ import multer from "multer";
 // Multer config
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "uploads/"),
-    filename: (req, file, cb) =>
-        cb(null, Date.now() + "-" + file.originalname),
-});
-const fileFilter = (req, file, cb) => {
-    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-    if (allowedFileTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => cb(null, "uploads/"),
+//     filename: (req, file, cb) =>
+//         cb(null, Date.now() + "-" + file.originalname),
+// });
+// const fileFilter = (req, file, cb) => {
+//     const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+//     if (allowedFileTypes.includes(file.mimetype)) {
+//         cb(null, true);
+//     } else {
+//         cb(null, false);
+//     }
+// }
 
-const upload = multer({ storage, fileFilter });
+// const upload = multer({ storage, fileFilter });
 
 router.post("/register", creatUser);
 router.get("/getAll", getUsers);
 router.post("/login", loginUser);
 router.patch("/update", updateProfile);
-router.post("/upload-profile", upload.single("profileImage"), uploadProfilePicture);
+// router.post("/upload-profile", upload.single("profileImage"), uploadProfilePicture);
 
 export default router;
