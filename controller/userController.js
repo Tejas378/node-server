fbe2597c406c7958de59a49afc97d3cc28de77b6
Tejas Pawar <tejaspawar172000@gmail.com>
2025-11-02 13:42:30 +0530

    Test

--- controller/userController.js
+++ controller/userController.js
@@ -161,14 +161,14 @@ export const uploadProfilePicture = async (req, res) => {
     }
     const { email } = req.body
     const result = await uploadImage(req, res)
-    const user = await User.findOneAndUpdate(
-      { email },
-      { profileImage: result?.secure_url },
-      { new: true } // returns updated document
-    );
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
+    // const user = await User.findOneAndUpdate(
+    //   { email },
+    //   { profileImage: result?.secure_url },
+    //   { new: true } // returns updated document
+    // );
+    // if (!user) {
+    //   return res.status(404).json({ error: "User not found" });
+    // }
     res.status(200).json(result)
 
 
