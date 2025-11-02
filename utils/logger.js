ebfafe2eeb59f63c6b5a5f97c9911b3bd51f9c74
Tejas Pawar <tejaspawar172000@gmail.com>
2025-11-02 10:59:05 +0530

    Added logging function

--- utils/logger.js
+++ utils/logger.js
@@ -2,12 +2,12 @@ import fs from "fs";
 import path from "path";
 
 // Ensure logs directory exists
-// const logDir = path.join(process.cwd(), "logs");
-// if (!fs.existsSync(logDir)) {
-//   fs.mkdirSync(logDir);
-// }
+const logDir = path.join(process.cwd(), "logs");
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
-// const logFile = path.join(logDir, "error.log");
+const logFile = path.join(logDir, "error.log");
 export const logErrorToFile = (error, context = "General") => {
   const timestamp = new Date().toISOString();
 
@@ -17,9 +17,9 @@ ${error instanceof Error ? error.stack : error}
 --------------------------------------------------
 `;
 
-  // fs.appendFile(logFile, message, (err) => {
-  //   if (err) {
-  //     console.error("Failed to write error log:", err);
-  //   }
-  // });
+  fs.appendFile(logFile, message, (err) => {
+    if (err) {
+      console.error("Failed to write error log:", err);
+    }
+  });
 };
